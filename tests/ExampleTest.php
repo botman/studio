dd320271779fa2a93f9800880856f09e8f231a3f
Katy Ereira <katye@deeson.co.uk>
2017-04-27 16:24:52 +0100

    Fix sample test.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $this->visit('/botman')
+            ->assertResponseStatus(200);
     }
 }
