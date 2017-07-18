f3592d15b3aa0f14d6597dbbc8a46d5a886aed2f
Florian Wartner <f.wartner@icloud.com>
2017-07-18 09:41:28 +0200

    Upgrade to L5.4

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Tinker\TinkerServiceProvider::class,
         \Mpociot\BotMan\BotManServiceProvider::class,
         \Mpociot\BotManTinker\BotManTinkerServiceProvider::class,
 
