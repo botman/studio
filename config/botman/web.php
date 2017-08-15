11b3dd7a2afa2bfa05308c2c4db2e785c980d183
Marcel Pociot <m.pociot@gmail.com>
2017-08-15 10:39:11 +0200

    Add web driver config

---
+++ config/botman/web.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web verification
+    |--------------------------------------------------------------------------
+    |
+    | This array will be used to match incoming HTTP requests against your
+    | web endpoint, to see if the request should match the web driver.
+    |
+    */
+    'matchingData' => [
+        'driver' => 'web',
+    ],
+];
