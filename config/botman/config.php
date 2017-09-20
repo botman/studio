c9db88ab722fec4eba90c00122afe512c22fe34a
Christoph Rumpel <c.rumpel@kabsi.at>
2017-09-20 20:49:09 +0200

    Add user cache time to config (#31)

--- config/botman/config.php
+++ config/botman/config.php
@@ -12,5 +12,17 @@ return [
     | the cache.
     |
     */
-    'conversation_cache_time' => 30
+    'conversation_cache_time' => 30,
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | BotMan caches user information of the incoming messages.
+    | This value defines the number of minutes that this
+    | data will remain stored in the cache.
+    |
+    */
+    'user_cache_time' => 30,
 ];
