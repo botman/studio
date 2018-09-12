c3316d97ed7124218b4fb169bc9044ba75ef7f09
Robin Valk <rjmvalk3@gmail.com>
2018-09-12 12:03:38 +0200

    Updated app config to 5.7

--- config/app.php
+++ config/app.php
@@ -10,6 +10,7 @@ return [
     | This value is the name of your application. This value is used when the
     | framework needs to place the application's name in a notification or
     | any other location as required by the application or its packages.
+    |
     */
 
     'name' => env('APP_NAME', 'Laravel'),
@@ -21,7 +22,7 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
-    | services your application utilizes. Set this in your ".env" file.
+    | services the application utilizes. Set this in your ".env" file.
     |
     */
 
@@ -149,15 +150,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
-
-        /*
-         * BotMan Service Providers...
-         */
-        BotMan\Tinker\TinkerServiceProvider::class,
-        App\Providers\BotMan\DriverServiceProvider::class,
-        BotMan\BotMan\BotManServiceProvider::class,
-        BotMan\Studio\Providers\StudioServiceProvider::class,
 
         /*
          * Application Service Providers...
