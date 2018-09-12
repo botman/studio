308a0d02768a92c4ec62d2ad00ed1d89b0b0c087
Robin Valk <rjmvalk3@gmail.com>
2018-09-12 13:56:01 +0200

    Re-added botman app service provider

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\BotMan\DriverServiceProvider::class,
     ],
 
     /*
