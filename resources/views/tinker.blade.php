03dc9f4b2629e4f2cc98e0f11a662b67391033ba
Robin Valk <valk3@hotmail.com>
2018-09-28 13:57:47 +0200

    Update to 5.7 -> Shift 9264 (#67)
    * Added missing maintenance middleware file
    
    * Updated views
    
    See: https://github.com/laravel/framework/pull/25442 for compatible meta tag removal
    
    * Updated app config to 5.7
    
    * Updated composer dependencies to match laravel, Upgraded botman studio-addons
    
    * Implemented email verification
    
    * Re-added botman app service provider

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>BotMan Studio</title>
 
